ee94ec0d2c0826b8e6db7aff3337039210bfdc48
TheDuckCow <theduckcow@live.com>
2018-03-30 21:40:21 -0400

    minor change to go back and enable original, full-sized UI preferences over condensed UI

--- __init__.py
+++ __init__.py
@@ -123,19 +123,18 @@ class DemoPreferences(bpy.types.AddonPreferences):
 
 		# updater draw function
 		# could also pass in col as third arg
-		# addon_updater_ops.update_settings_ui(self, context)
+		addon_updater_ops.update_settings_ui(self, context)
 
 		# Alternate draw function, which is more condensed and can be 
 		# placed within an existing draw function. Only contains:
 		#   1) check for update/update now buttons
 		#   2) toggle for auto-check (interval will be equal to what is set above)
-		addon_updater_ops.update_settings_ui_condensed(self, context, col)
-		
+		# addon_updater_ops.update_settings_ui_condensed(self, context, col)
+
 		# Adding another column to help show the above condensed ui as one column
-		col = mainrow.column()
-		col.scale_y = 2
-		col.operator("wm.url_open",
-				"Open webpage ").url=addon_updater_ops.updater.website
+		# col = mainrow.column()
+		# col.scale_y = 2
+		# col.operator("wm.url_open","Open webpage ").url=addon_updater_ops.updater.website
 
 
 def register():
