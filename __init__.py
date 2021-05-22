d21ab250acc359f8706df3f844bf4e884c83a473
TheDuckCow <theduckcow@live.com>
2021-05-22 16:37:22 -0400

    Upversion and updated author credits

--- __init__.py
+++ __init__.py
@@ -19,8 +19,8 @@
 bl_info = {
 	"name":        "Addon Updater Demo",
 	"description": "Demo addon for showcasing the blender-addon-updater module",
-	"author":      "Patrick W. Crawford",
-	"version":     (1, 0, 9),
+	"author":      "Patrick W. Crawford, neomonkeus",
+	"version":     (1, 1, 0),
 	"blender":     (2, 80, 0),
 	"location":    "View 3D > Tool Shelf > Demo Updater",
 	"warning":     "",  # used for warning icon and text in addons panel
